7747d04628c7e45ba0307d425963388b4d2ac797
Celse13 <rugiracelse13@gmail.com>
2024-01-06 02:43:54 -0800

    Fixing errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """Index Blueprint"""
-
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
+    "Returning the status of the API upon the get request."
     return jsonify(status='OK')
