86093bf9f6c0129634964b7bb1888521a5a9d162
Dru Mumba <drusademumba@gmail.com>
2024-01-07 15:15:21 +0300

    Add api routes prototypes

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Handles all default RESTFul API actions for users"""
+from api.v1.views import app_views
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def list_all_users():
+    """List all users"""
+    pass
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def retrieve_a_user(user_id):
+    """Retrieve a user"""
+    pass
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_a_user(user_id):
+    """Delete a user"""
+    pass
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_a_user():
+    """Create a user"""
+    pass
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_a_user(user_id):
+    """Update a user"""
+    pass
