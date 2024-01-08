e99730fd8c1f3cf5ab59e86a7af2423ff7bfe19c
Dru Mumba <drusademumba@gmail.com>
2024-01-08 22:26:45 +0300

    tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -16,14 +16,14 @@ class TestConsoleDocs(unittest.TestCase):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
+        self.assertEqual(result.total_errors, 1,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
+        self.assertEqual(result.total_errors, 1,
                          "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
